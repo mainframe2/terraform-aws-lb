5c339fb057a420bc6768e5a8e09e2799b01d716a
Stefan Mihajlovic <stefan.mihajlovic@fra.me>
2019-07-31 22:48:08 +0200

    Change var names

--- modules/lb_attachment/main.tf
+++ modules/lb_attachment/main.tf
@@ -1,10 +1,10 @@
 locals {
-  total_count_attachments = var.number_of_instances * length(var.listener)
-  total_count_listeners   = length(var.listener)
+  attachments_total_count = var.number_of_instances * length(var.listener)
+  listeners_total_count   = length(var.listener)
 }
 
 resource "aws_lb_target_group" "this" {
-  count    = local.total_count_listeners
+  count    = local.listeners_total_count
   name     = "${var.name}-${count.index}"
   port     = element(var.listener, count.index)["instance_port"]
   protocol = element(var.listener, count.index)["instance_protocol"]
@@ -21,14 +21,14 @@ resource "aws_lb_target_group" "this" {
 }
 
 resource "aws_lb_target_group_attachment" "this" {
-  count            = local.total_count_attachments
-  target_group_arn = element(aws_lb_target_group.this.*.arn, floor("${count.index / local.total_count_listeners}"))
+  count            = local.attachments_total_count
+  target_group_arn = element(aws_lb_target_group.this.*.arn, floor("${count.index / local.listeners_total_count}"))
   target_id        = "${element(var.instances, count.index % length(var.instances))}"
   port             = "${element(var.listener, floor(count.index / length(var.instances)))["instance_port"]}"
 }
 
 resource "aws_lb_listener" "this" {
-  count             = local.total_count_listeners
+  count             = local.listeners_total_count
   load_balancer_arn = var.load_balancer_arn
   port              = element(var.listener, count.index)["instance_port"]
   protocol          = element(var.listener, count.index)["instance_protocol"]
