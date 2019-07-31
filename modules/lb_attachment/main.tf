8274c7945c862286b70e25a982ff0513b8ac7198
Stefan Mihajlovic <stefan.mihajlovic@fra.me>
2019-07-31 22:41:07 +0200

    Add health-check support

--- modules/lb_attachment/main.tf
+++ modules/lb_attachment/main.tf
@@ -4,10 +4,18 @@ locals {
 }
 
 resource "aws_lb_target_group" "this" {
-  count             = local.total_count_listeners
-  name              = var.name
-  port              = element(var.listener, count.index)["instance_port"]
-  protocol          = element(var.listener, count.index)["instance_protocol"]
+  count    = local.total_count_listeners
+  name     = var.name
+  port     = element(var.listener, count.index)["instance_port"]
+  protocol = element(var.listener, count.index)["instance_protocol"]
+
+  health_check = {
+    interval            = 5
+    protocol            = TCP
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
   vpc_id            = var.vpc_id
   proxy_protocol_v2 = var.enable_proxy_protocol
 }
