46a051ce376a780ff39a84cd13c961e9901f786b
smihajlovic-frame <stefan.mihajlovic@fra.me>
2019-11-07 17:06:36 +0100

    Move away from tuple to maps

--- modules/lb/main.tf
+++ modules/lb/main.tf
@@ -11,7 +11,7 @@ resource "aws_lb" "this" {
   enable_deletion_protection = var.enable_deletion_protection
 
   dynamic "subnet_mapping" {
-    for_each = var.internal ? [] : var.subnet_mapping
+    for_each = var.internal ? {} : var.subnet_mapping
     content {
       subnet_id     = subnet_mapping.key
       allocation_id = subnet_mapping.value
