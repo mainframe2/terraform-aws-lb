22e7560669d5e57f64c7777b9c19846c72d105d2
smihajlovic-frame <stefan.mihajlovic@fra.me>
2019-12-10 00:11:26 +0100

    Add health-check port customization for NLBs

--- modules/lb_attachment/main.tf
+++ modules/lb_attachment/main.tf
@@ -15,6 +15,7 @@ resource "aws_lb_target_group" "this" {
     interval            = 10
     healthy_threshold   = 2
     unhealthy_threshold = 2
+    port                = element(var.listener, count.index)["health_check_port"]
   }
 
   vpc_id            = var.vpc_id
